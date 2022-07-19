406fa54f5eb710adfb234f5620a9a732b9f9e1cb
Jonjo7Dia <jonosphgames@gmail.com>
2022-07-19 18:25:51 -0300

    ready to implement hamburger bar

--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -1,44 +1,62 @@
 import { useState } from "react";
 import "./Nav.css";
 function Nav() {
-  const [isActive, setIsActive] = useState('Home');
-  
+  const [isActive, setIsActive] = useState("Home");
+  console.log(isActive);
   return (
     <div className={"nav"}>
-      <div className={"navHeading"} onClick={()=>{
-        setIsActive('Home');
-      }}>
+      <div
+        className={"navHeading"}
+        onClick={() => {
+          setIsActive("Home");
+        }}
+      >
         <div className={"title"}>
           <h1>Bar Training</h1>
         </div>
-        <span className={"underline"}></span>
+        <span className={"underline"} style={{width: isActive === 'Home' ? '98%' : '0%'}}></span>
       </div>
       <div className={"navLinks"}>
         <ul>
           <li>
-            <div className={"navItem"}>
+            <div
+              className={"navItem"}
+              onClick={() => {
+                setIsActive("Flashcards");
+              }}
+            >
               <div className={"title"}>
                 <h3>Flashcards</h3>
               </div>
-              <span className={"underline"}></span>
+              <span className={"underline"} style={{width: isActive === 'Flashcards' ? '98%' : '0%',}}></span>
             </div>
           </li>
           <li>
-            <div className={"navItem"}>
+            <div
+              className={"navItem"}
+              onClick={() => {
+                setIsActive("History");
+              }}
+            >
               <div className={"title"}>
                 <h3>Cocktail History</h3>
               </div>
 
-              <span className={"underline"}></span>
+              <span className={"underline"}  style={{width: isActive === 'History' ? '98%' : '0%'}}></span>
             </div>
           </li>
           <li>
-            <div className={"navItem"}>
+            <div
+              className={"navItem"}
+              onClick={() => {
+                setIsActive("Quiz");
+              }}
+            >
               <div className={"title"}>
                 <h3>Quiz</h3>
               </div>
 
-              <span className={"underline"}></span>
+              <span className={"underline"} style={{width: isActive === 'Quiz' ? '98%' : '0%'}}></span>
             </div>
           </li>
         </ul>
