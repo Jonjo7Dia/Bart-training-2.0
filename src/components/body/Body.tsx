98ad018d94a3827077a32cf91f0a7ef8899459e7
Jonjo7Dia <jonosphgames@gmail.com>
2022-07-20 18:52:54 -0300

    ready to implement home

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,11 +1,13 @@
 import "./Body.css";
 import Background from "./Background";
+import Home from '../Home/Home';
 function Body(props: any) {
   return (
     <div className={"Body"}>
       <Background />
 
       {props.children}
+      <Home />
     </div>
   );
 }
