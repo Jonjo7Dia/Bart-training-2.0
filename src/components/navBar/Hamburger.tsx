4b820aa9dace8f04c6f6727cd631a24f7b2c37b7
Jonjo7Dia <jonosphgames@gmail.com>
2022-07-20 15:49:31 -0300

    mobile nav css implemented

--- src/components/navBar/Hamburger.tsx
+++ src/components/navBar/Hamburger.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './Hamburger.css';
+import MobileNavItems from './MobileNavItems';
 function Hamburger(){
-    return <div className={'mobileNav'}>
+    const [isOpen, setIsOpen] = useState(false);
+    return <div className={`mobileNav ${isOpen? 'open' : ''}`} onClick={()=>{
+        setIsOpen(!isOpen);
+    }}>
         <div className="mobile-nav_burger">
-            
+
         </div>
+       { isOpen && <div className={'mobileNavItems'}>
+           <MobileNavItems/></div>}
     </div>
 }
 
---
+++ src/components/navBar/MobileNavItems.tsx
@@ -0,0 +1,13 @@
+import "./MobileNavItems.css";
+function MobileNavItems() {
+  return (
+    <div className="mobileNavList">
+      <ul>
+        <li className="firstItem">Flashcards</li>
+        <li className="secondItem">Cocktail History</li>
+        <li  className="thirdItem">Quiz</li>
+      </ul>
+    </div>
+  );
+}
+export default MobileNavItems;
