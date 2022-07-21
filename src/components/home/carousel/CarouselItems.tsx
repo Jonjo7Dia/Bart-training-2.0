a1e5b234d9019fba2039e91cd887ac1d409afada
Jonjo7Dia <jonosphgames@gmail.com>
2022-07-21 16:05:08 -0300

    must know cards implemented

--- src/components/home/carousel/CarouselItems.tsx
+++ src/components/home/carousel/CarouselItems.tsx
@@ -1,11 +1,16 @@
 import './CarouselItems.css'
 import DrinkCard from './DrinkCard';
+import { useSelector } from "react-redux";
+import { PageState } from "../../../store/pagesReducer";
+
 function CarouselItems(){
+    const drinks= useSelector<PageState, PageState["drinks"]>(state=>state.drinks);
+
+    const mustKnow = [drinks[9],drinks[8], drinks[6], drinks[4]];
+    console.log(mustKnow);
     return <div className={'carouselItems'}>
-        <DrinkCard />
-        <DrinkCard />
-        <DrinkCard />
-        <DrinkCard />
+        {mustKnow.map((drink, index) => (<DrinkCard key={index} drink={drink}/>))}
+
     </div>
 }
 
--- src/components/home/carousel/DrinkCard.tsx
+++ src/components/home/carousel/DrinkCard.tsx
@@ -1,10 +1,27 @@
 import './DrinkCard.css'
-import tea from  '../../../images/Negroni.png'
-function DrinkCard(){
+import tea from  '../../../images/Martini.png';
+
+interface Props {
+    drink: {
+      Name: string;
+      Ingredients: string[];
+      Recipe: string[];
+      History: string;
+    }
+  }
+function DrinkCard({drink} : Props){
+    let Name = drink.Name;
+    if(Name === 'Martini Gin/Vodka 6:1'){
+        Name = 'Martini';
+    }
+    const source = require(`../../../images/${Name}.png`)
+    console.log(Name);
+    console.log(source);
     return <div className={'drinkCard'}>
-        <div className={'drinkImage'}><img src={tea} alt="long island"/>
+        <div className={'drinkImage'}><img src={source} alt={Name} 
+/>
 </div>
-        <div className={'drinkName'}><h3>Long Island Ice Tea</h3></div>
+        <div className={'drinkName'}><h3>{Name}</h3></div>
     </div>
 }
 
