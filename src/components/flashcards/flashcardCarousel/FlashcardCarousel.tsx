6ecb9711b41c23b5e50e78bf0f214ab6186ffa9e
Jonjo7Dia <jonosphgames@gmail.com>
2022-07-23 20:31:38 -0300

    ready for options design

--- src/components/flashcards/flashcardCarousel/FlashcardCarousel.tsx
+++ src/components/flashcards/flashcardCarousel/FlashcardCarousel.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 function FlashcardCarousel() {
   const [card, setCard] = useState(0);
   let items: any = [];
+  let itemsRight: any = [];
   const drinks = useSelector<PageState, PageState["drinks"]>(
     (state) => state.drinks
   );
@@ -19,22 +20,37 @@ function FlashcardCarousel() {
   });
   function nextCard() {
     const currentElement = document.getElementById(`fcCard${card}`);
-    const nextElement = document.getElementById(`fcCard${card + 1}`);
-
-    nextElement?.classList.add("next");
-    currentElement?.classList.add('back');
+    currentElement?.classList.remove("back", "next", "backRight", "nextRight");
+    currentElement?.classList.add("back");
     if (card === 18) {
+      const nextElement = document.getElementById(`fcCard0`);
+      nextElement?.classList.remove("back", "next", "backRight", "nextRight");
+      nextElement?.classList.add("next");
       setCard(0);
     } else {
+      const nextElement = document.getElementById(`fcCard${card + 1}`);
+      nextElement?.classList.remove("back", "next", "backRight", "nextRight");
+      nextElement?.classList.add("next");
       setCard((prevstate) => {
         return prevstate + 1;
       });
     }
   }
   function lastCard() {
+    const currentElement = document.getElementById(`fcCard${card}`);
+    currentElement?.classList.remove("back", "next", "backRight", "nextRight");
+
+    currentElement?.classList.add("backRight");
     if (card === 0) {
-      setCard(drinks.length - 1);
+      const nextElement = document.getElementById(`fcCard18`);
+      nextElement?.classList.remove("back", "next", "backRight", "nextRight");
+      nextElement?.classList.add("nextRight");
+      setCard(18);
     } else {
+      const nextElement = document.getElementById(`fcCard${card - 1}`);
+      nextElement?.classList.remove("back", "next", "backRight", "nextRight");
+      nextElement?.classList.add("nextRight");
+
       setCard((prevstate) => {
         return prevstate - 1;
       });
@@ -42,9 +58,7 @@ function FlashcardCarousel() {
   }
   return (
     <div className={"fcCarousel"}>
-      <div className={"cardHolder"}>
-        {items}
-      </div>
+      <div className={"cardHolder"}>{items}</div>
       <CarouselOptions next={nextCard} back={lastCard} />
     </div>
   );
